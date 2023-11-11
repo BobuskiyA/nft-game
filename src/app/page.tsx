4826951cc13499662d1c188941ab95bf9e680834
Bobuskiy <bobuskiy.olexander@example.com>
2023-11-11 23:10:07 +0100

    Add future block & fix class .big-text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { ScrollProvider } from '../helpers/scrollProvider'
 import Hero from '@/sections/Hero/Hero'
 import AboutProject from '@/sections/AboutProject/AboutProject'
 import Story from '@/sections/Story/Story'
+import Future from '@/sections/Future/Future'
 
 export default function Home() {
 
@@ -13,7 +14,7 @@ export default function Home() {
         <Header />
 
         <Hero />
-
+        <Future/>
         <AboutProject />
 
         <Story />
